e4ac698782d939e9c1391463d6222b12b15b2f45
natan borges <natanmelo5@hotmail.com>
2023-10-25 19:27:15 -0300

    first commit

---
+++ src/model/Discount.ts
@@ -0,0 +1,8 @@
+import {Product} from "./Product"
+
+export class Discount {
+
+    constructor(public readonly product: Product,
+    public readonly description: string,
+    public readonly discountAmount: number) {}
+}
